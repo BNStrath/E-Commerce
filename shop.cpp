11a7e13023029c7013351b63180e52391deabe1f
BNStrath <157800335+BNStrath@users.noreply.github.com>
2025-03-18 14:07:06 +0000

    Add files via upload

---
+++ shop.cpp
@@ -0,0 +1,15 @@
+#include "item.h"
+
+void addToCart(vector<Item*> items, Item* item) {
+	items.push_back(item);
+}
+
+void deleteFromCart(vector<Item*> items, string id) {
+	for (Item* item : items) {
+		if (item->id == id) {
+			items.push_back(item);
+			items.pop_back();
+		}
+	}
+}
+
